f923285ffd6ec65405cf08392ff4d5778d652769
adminMENA <admin@me-s.org>
2024-12-20 05:43:32 +0300

    Fix webhook handling

--- bot.py
+++ bot.py
@@ -1,35 +1,29 @@
 from telegram.ext import ApplicationBuilder, MessageHandler
 from telegram import Update
 from telegram.ext import ContextTypes, filters
-import os  # استيراد مكتبة os للحصول على المنفذ من البيئة
+import os 
 
-# ضع رمز الـ API الخاص بك هنا
 TOKEN = "6243650306:AAHSCTWLd2nNQhTtFOIfhH2puFqU3eVUzcA"
 
-# الدالة التي تتعامل مع الرسائل
 async def auto_reply(update: Update, context: ContextTypes.DEFAULT_TYPE):
     if update.message:  # تحقق من وجود رسالة
         reply = "https://t.me/mena_support تواصل هنا فقط"
         await update.message.reply_text(reply)
 
-# إعداد البوت
 def main():
-    # إنشاء التطبيق وربط التوكن
     app = ApplicationBuilder().token(TOKEN).build()
 
-    # إضافة معالج للرسائل النصية
     app.add_handler(MessageHandler(filters.TEXT & (~filters.COMMAND), auto_reply))
 
     print("البوت يعمل الآن...")
 
-    # الحصول على المنفذ من البيئة
-    port = int(os.environ.get('PORT', 8443))
+    port = int(os.environ.get('PORT', 8888))
 
     # إعداد Webhook
     app.run_webhook(
         listen="0.0.0.0",
         port=port,
-        webhook_url=f"https://bot.onrender.com/{TOKEN}"  # الرابط المعدّل ليطابق اسم تطبيقك
+        webhook_url=f"https://bot-gdg8.onrender.com/{TOKEN}" 
     )
 
 if __name__ == "__main__":
